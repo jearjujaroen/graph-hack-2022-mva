c9414b2588caa2451e65edab1949513740dc7ca8
jearjujaroen <jear.jujaroen@gmail.com>
2022-06-05 02:19:14 -0700

    clean up and fix build error

--- fetch/account.ts
+++ fetch/account.ts
@@ -1,28 +1,15 @@
-import {
-	Address,
-} from '@graphprotocol/graph-ts'
+import { Address, BigInt, log } from '@graphprotocol/graph-ts';
 
-import {
-	Account,
-} from '../generated/schema'
+import { Account } from '../generated/schema';
 
-import { integer } from '@protofire/subgraph-toolkit'
+import { integer } from '@protofire/subgraph-toolkit';
 
 export function fetchAccount(address: Address): Account {
-	let account = Account.load(address);
-	if(account == null){
-		account = new Account(address)
-		account.totalTransactions = integer.ONE
-		account.save()
-	} 
-	else {
-		// log.debug('Block number: {}, block hash: {}, transaction hash: {}', [
-		// 	event.block.number.toString(), // "47596000"
-		// 	event.block.hash.toHexString(), // "0x..."
-		// 	event.transaction.hash.toHexString(), // "0x..."
-		//   ])
-		account.totalTransactions = integer.increment(integer.ONE)
-		account.save()
-	}
-	return account
+  let account = Account.load(address);
+  if (account == null) {
+    account = new Account(address);
+    account.totalTransactions = integer.ZERO;
+    account.save();
+  }
+  return account;
 }
